43c9accfa536bfea2d380127b2dba015c4e14ca8
Troels Knak-Nielsen <troelskn@gmail.com>
2009-04-19 20:51:23 +0000

    Wire - A dependency injection containert

---
+++ lib/k/registry.php
@@ -0,0 +1,9 @@
+<?php
+/**
+  * A combined singleton registry and general purpose class factory
+  * k_Registry has been replaced with k_wire_Container
+  * @deprecated
+  */
+class k_Registry extends k_wire_Container
+{
+}
